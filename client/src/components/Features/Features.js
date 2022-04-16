4e67310533a1aeab594c0649b0d2bd810fcd1e43
ducodedao <tongodu95@gmail.com>
2022-04-16 11:37:43 +0700

    Design Features

--- client/src/components/Features/Features.js
+++ client/src/components/Features/Features.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import './Features.css'
+import { BsFillBookmarkStarFill } from 'react-icons/bs'
+import phoneFeatures from '../../assets/phone-features.png'
+import Feature from './Feature'
+import { FeatureList } from './data'
 
 const Features = () => {
-    return <div>Features</div>
+    return (
+        <section id="features">
+            <div className="container features">
+                <div className="title" data-aos="fade-up">
+                    <BsFillBookmarkStarFill color="orangered" size={30} />
+                    <h2>Core Features</h2>
+                    <p className="u-text-small">
+                        SocialX app has Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Sequi labore, harum officiis error
+                        quia cupiditate?
+                    </p>
+                </div>
+                <div className="features-content">
+                    <div className="features-left" data-aos="fade-right">
+                        <img src={phoneFeatures} alt="phone" />
+                    </div>
+                    <div className="features-right" data-aos="fade-left">
+                        {FeatureList.map((feature) => (
+                            <Feature
+                                key={feature.id}
+                                icon={feature.icon}
+                                heading={feature.heading}
+                                text={feature.text}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Features
